3cd4dd963264c923df39a5f04ff22eb690510e35
Caitlyn Larson <124397489+larsocai@users.noreply.github.com>
2023-10-27 15:11:54 -0700

    Update dishList.jsx

--- client/components/DishList.jsx
+++ client/components/DishList.jsx
@@ -26,9 +26,6 @@ const QUERY = gql`
 `;
 
 function DishCard({ data }) {
-  const baseUrl = process.env.STRAPI_URL || "https://strapi-g45m.onrender.com";
-  const imageUrl = `${baseUrl}${data.attributes.image.data.attributes.url}`;
-
   return (
     <div className="w-full md:w-1/2 lg:w-1/3 p-4">
       <div className="h-full bg-gray-100 rounded-2xl">
@@ -36,7 +33,9 @@ function DishCard({ data }) {
           className="w-full rounded-2xl"
           height={300}
           width={300}
-          src={imageUrl} // Use the constructed imageUrl here
+          src={`${process.env.STRAPI_URL || "https://strapi-g45m.onrender.com"}${
+            data.attributes.image.data.attributes.url
+          }`}
           alt=""
         />
         <div className="p-8">
@@ -61,42 +60,6 @@ function DishCard({ data }) {
     </div>
   );
 }
-
-// function DishCard({ data }) {
-//   return (
-//     <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-//       <div className="h-full bg-gray-100 rounded-2xl">
-//         <Image
-//           className="w-full rounded-2xl"
-//           height={300}
-//           width={300}
-//           src={`${process.env.STRAPI_URL || "https://strapi-g45m.onrender.com"}${
-//             data.attributes.image.data.attributes.url
-//           }`}
-//           alt=""
-//         />
-//         <div className="p-8">
-//           <h3 className="mb-3 font-heading text-xl text-gray-900 hover:text-gray-700 group-hover:underline font-black">
-//             {data.attributes.name}
-//           </h3>
-//           <p className="text-sm text-gray-500 font-bold">
-//             {data.attributes.description}
-//           </p>
-//           <div className="flex flex-wrap md:justify-center -m-2">
-//             <div className="w-full md:w-auto p-2 my-6">
-//               <Link
-//                 className="block w-full px-12 py-3.5 text-lg text-center text-white font-bold bg-gray-900 hover:bg-gray-800 focus:ring-4 focus:ring-gray-600 rounded-full"
-//                 href={`/dish/${data.id}`}
-//               >
-//                 View
-//               </Link>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 function DishList(props) {
   const { loading, error, data } = useQuery(QUERY);
